55ef57b2fd83c68b59162b7f062654e76a92bdc6
Tilmann <zoodb@gmx.de>
2017-01-08 15:24:29 +0100

    Fixed bug in old CritBit code

--- src/main/java/org/zoodb/index/critbit/CritBit.java
+++ src/main/java/org/zoodb/index/critbit/CritBit.java
@@ -46,6 +46,10 @@ package org.zoodb.index.critbit;
  * BitTools.toSortableLong(...), also when supplying query parameters.
  * Extracted values can be converted back with BitTools.toDouble() or toFloat().
  * 
+ * Version 1.3.5 
+ * - Fixed rare problem with postfix creation. This solves a problem
+ *   with kd-queries and slightly reduces memory consumption.
+ * 
  * Version 1.3.2
  * - Added QueryIterator.reset()
  * 
@@ -369,7 +373,7 @@ public class CritBit<V> implements CritBit1D<V>, CritBitKD<V> {
 	 * @return the postfix.
 	 */
 	private long[] createPostFix(long[] val, int posDiff) {
-		int preLen = posDiff >>> 6;
+		int preLen = (posDiff+1) >>> 6;
 		long[] p = new long[val.length - preLen];
 		System.arraycopy(val, preLen, p, 0, p.length);
 		return p;
