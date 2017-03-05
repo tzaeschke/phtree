11b02088230afb259cb067eadefeed784a81a422
Tilmann <zoodb@gmx.de>
2017-03-05 19:26:37 +0100

    Updated tuning suggestions

--- src/main/java/ch/ethz/globis/phtree/v11/Node.java
+++ src/main/java/ch/ethz/globis/phtree/v11/Node.java
@@ -32,6 +32,7 @@ public class Node {
 	private static final int REF_BITS = 4*8;
 	private static final int HC_BITS = 0;  //number of bits required for storing current (HC)-representation
 	private static final int INN_HC_WIDTH = 0; //Index-NotNull: width of not-null flag for post/infix-hc
+	/** Bias towards using AHC. AHC is used if: sizeLHC*AHC_LHC_BIAS >= sizeAHC  */
 	public static final double AHC_LHC_BIAS = 2.0; 
 	public static final int NT_THRESHOLD = 150; 
 
